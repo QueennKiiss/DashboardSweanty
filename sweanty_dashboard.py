fba4985b1fde4d487ee44dfc880ef21c80b716e6
mmaestro <mmaestro@circontrol.com>
2022-08-18 20:40:59 +0200

    Implement a simplified main app module

---
+++ sweanty_dashboard.py
@@ -0,0 +1,34 @@
+import dash
+from dash import dcc
+from dash import html
+import dash_bootstrap_components as dbc
+from source import layout_components, callbacks
+
+
+def main() -> None:
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ])
+    # Build App layout
+    app.layout = html.Div(
+        id='body',
+        className="body",
+        children=[
+            dcc.Interval(
+                id='interval_data',
+                interval=1 * 2000,  # in milliseconds
+                n_intervals=0
+                ),
+            # HEADER
+            layout_components.header(app),
+            # Main Content
+            layout_components.main_content(app),
+            # FOOTER
+            layout_components.footer(app),
+            ]
+        )
+    callbacks.update_volunteer_data(app)
+    callbacks.update_input_data(app)
+    app.run_server(debug=True)
+
+
+if __name__ == "__main__":
+    main()
