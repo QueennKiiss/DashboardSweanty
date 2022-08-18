d759c06d3fa5dff19a22a907400aabc6eb82671d
mmaestro <mmaestro@circontrol.com>
2022-08-18 20:41:41 +0200

    Implement module for image references

---
+++ resources/images.py
@@ -0,0 +1,6 @@
+
+
+# Images and Icons
+USER_PROFILE_IMG = app.get_asset_url("img/user_profile.png")
+SWEANTY_ICON = app.get_asset_url("img/logo_sweanty_1-184x81.png")
+BEWOLFISH_ICON = app.get_asset_url("img/bewolfish_logo_color.png")
