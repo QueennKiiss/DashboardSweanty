709d4eaaf23c0e7d9cdf173940eba042d4e3690a
mmaestro <mmaestro@circontrol.com>
2022-11-25 12:27:01 +0100

    Refactor: Improve code appearance

--- source/layout_components.py
+++ source/layout_components.py
@@ -16,11 +16,11 @@ def header(app: Dash) -> html.Div:
                 ),
             html.Div(
                 className="cell",
-                children=[html.H1(className="header_title", children="SWEANTY-BEWOLFISH STUDY")],
+                children=[html.H1(className="header_title", children="SWEANTY STUDY")],
                 ),
             html.Div(
                 className="cell",
-                children=[html.Img(className="icon_right", src=bewolfish_icon)]
+                children=[html.Img(className="icon_right", src=sweanty_icon)]
                 ),
             ]
         )
@@ -48,9 +48,7 @@ def right_column(app: Dash) -> html.Div:
         children=[
             html.Div(
                 className="sub_header",
-                children=html.Div(
-                    html.H2("USER STATISTICS"),
-                    )
+                children=html.H2("USER STATISTICS"),
                 ),
             html.Div(
                 className="matrix_graph",
@@ -58,28 +56,36 @@ def right_column(app: Dash) -> html.Div:
                     html.Div(
                         className="graph_box",
                         children=dcc.Graph(
-                            className="dcc_graph", id="salt_losses", config={"displayModeBar": False},
+                            className="dcc_graph",
+                            id="salt_losses",
+                            config={"displayModeBar": False},
                             # figure=salt_loss_figure(0),
                             ),
                         ),
                     html.Div(
                         className="graph_box",
                         children=dcc.Graph(
-                            className="dcc_graph", id="sweat_id", config={"displayModeBar": False},
+                            className="dcc_graph",
+                            id="sweat_id",
+                            config={"displayModeBar": False},
                             # figure=sweat_id_figure(0),
                             ),
                         ),
                     html.Div(
                         className="dcc_graph_two_columns",
                         children=dcc.Graph(
-                            className="dcc_graph", id="weight_graph", config={"displayModeBar": False},
+                            className="dcc_graph",
+                            id="weight_graph",
+                            config={"displayModeBar": False},
                             # figure=weight_figure(None, 0, 0)
                             )
                         ),
                     html.Div(
                         className="dcc_graph_three_columns",
                         children=dcc.Graph(
-                            id="bar_graph", className="dcc_graph", config={"displayModeBar": False},
+                            id="bar_graph",
+                            className="dcc_graph",
+                            config={"displayModeBar": False},
                             # figure=salt_amount([0], [0])
                             ),
                         ),
@@ -95,9 +101,14 @@ def left_column(app: Dash) -> html.Div:
     return html.Div(
         className='leftcolumn',
         children=[
-            html.H2("USER PROFILE", style={'textAlign': 'center', 'color': 'white'}),
+            html.H2(
+                "USER PROFILE",
+                # style={'textAlign': 'center', 'color': 'white'}
+                ),
             dcc.Dropdown(
-                id='user', className="user_dropdown", options=data.get_plot_data()['user_name'],
+                id='user',
+                className="user_dropdown",
+                options=data.get_plot_data()['user_name'],
                 placeholder="Select a user..."
                 ),
             html.Img(className="user_image", src=user_profile_img, ),
@@ -118,11 +129,10 @@ def footer(app: Dash) -> html.Div:
     return html.Div(
         className="footer_container",
         children=[
-            html.Div(
-                children=[
-                    html.H4("Footer section"),
-                    html.P("This is the final part of the Dashboard, like a footer but using a paragraph html element"),
-                    ]
-                )
-            ],
+            html.H4("Footer section"),
+            html.P(
+                "This is the final part of the Dashboard, "
+                "like a footer but using a paragraph html element"
+                ),
+            ]
         )
