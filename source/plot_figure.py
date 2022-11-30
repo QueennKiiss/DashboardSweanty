c362fe820d305b0d71cda5b14fba06f000ffc7e7
mmaestro <mmaestro@circontrol.com>
2022-11-30 13:46:35 +0100

    Feat: Add options to update layout figures

--- source/plot_figure.py
+++ source/plot_figure.py
@@ -11,19 +11,12 @@ def sweat_id_figure(data):
         mode="number",
         value=data,
         domain={'x': [0, 1], 'y': [0, 1]},
+        number={'font': {'size': 75}}
         ))
     fig.update_layout(
         font={'color': "white", 'family': "Arial"},
-        margin=dict(l=0, r=0, t=50, b=10, pad=0),
-        paper_bgcolor='cadetblue',
-        title={
-            'text': "Sweat ID",
-            'font': {'size': 30},
-            'xref': 'container',
-            'x': 0.5,
-            }
+        paper_bgcolor='darkcyan',
         )
-
     return fig
 
 
@@ -52,57 +45,8 @@ def salt_loss_figure(data):
         )
     fig.update_layout(
         font={'color': "white", 'family': "Arial"},
-        title={
-            'text': "Tendency to lose salt",
-            'font': {'size': 30},
-            'xref': 'container',
-            'x': 0.1,
-            },
-        margin=dict(l=10, r=10, t=50, b=50, pad=4),
-        paper_bgcolor='cadetblue',
-        )
-    return fig
-
-
-def salt_amount(users, salt_amounts):
-    fig = go.Figure()
-    fig.add_trace(
-        go.Bar(
-            x=users, y=salt_amounts,
-            name='base_cond',
-            marker_color='lightblue'
-            )
-        )
-    fig.update_layout(
-        # height=500,
-        margin=dict(l=20, r=20, t=40, b=20),
-        title={
-            'text': 'Salt amount',
-            'font': {'size': 30},
-            'xref': 'container',
-            'x': 0.5,
-            'y': 0.98,
-            },
-        xaxis_tickfont_size=14,
-        xaxis=dict(
-             tickfont_size=18,
-            ),
-        yaxis=dict(
-            title='Salt amount (a.u.)',
-            titlefont_size=22,
-            tickfont_size=18,
-            ),
-        legend=dict(
-            x=0, y=0.9,
-            bordercolor='black',
-            borderwidth=1
-            ),
-        barmode='group',
-        bargap=0.15,  # gap between bars of adjacent location coordinates.
-        bargroupgap=0.1,  # gap between bars of the same location coordinate.
-        paper_bgcolor='cadetblue',
-        plot_bgcolor='cadetblue',
-        font={'color': 'white'}
+        margin=dict(l=10, r=10, t=0, b=50, pad=4),
+        paper_bgcolor='darkcyan',
         )
     return fig
 
@@ -124,27 +68,58 @@ def weight_figure(user, weight_before, weight_after):
             )
         )
     fig.update_layout(
-        font={'color': "white", 'family': "Arial"},
-        title={
-            'text': "Weight comparison",
-            'font': {'size': 30},
-            'xref': 'container',
-            'x': 0.5,
-            },
+        font={'color': "black", 'family': "Arial"},
         xaxis=dict(
-            tickfont_size=20,
+            tickfont_size=22,
             ),
         yaxis=dict(
             title='Weight (kg)',
             titlefont_size=22,
-            tickfont_size=20,
-            range=[0, 100]
+            tickfont_size=18,
+            range=[0, 100],
+            ticks="outside",
+            tickwidth=2,
+            tickcolor='black',
+            ticklen=10
             ),
         legend=dict(
-            font={'size': 20}
+            font={'size': 16},
+            x=0.67, y=0.99
             ),
-        margin=dict(l=0, r=5, t=50, b=50, pad=4),
-        paper_bgcolor='cadetblue',
-        plot_bgcolor='cadetblue',
+        margin=dict(l=0, r=5, t=0, b=0, pad=0),
+        paper_bgcolor='darkcyan',
+        # plot_bgcolor='cadetblue',
+        )
+    return fig
+
+
+def salt_amount(users, salt_amounts):
+    fig = go.Figure()
+    fig.add_trace(
+        go.Bar(
+            x=users, y=salt_amounts,
+            name='base_cond',
+            marker_color='lightblue',
+            )
+        )
+    fig.update_layout(
+        margin=dict(l=0, r=0, t=0, b=0),
+        xaxis=dict(tickfont_size=18,),
+        yaxis=dict(
+            title='Salt amount (a.u.)',
+            titlefont_size=22,
+            tickfont_size=18,
+            range=[0, 80],
+            ticks="outside",
+            tickwidth=2,
+            tickcolor='black',
+            ticklen=10
+            ),
+        barmode='group',
+        bargap=0.15,  # gap between bars of adjacent location coordinates.
+        bargroupgap=0.1,  # gap between bars of the same location coordinate.
+        paper_bgcolor='darkcyan',
+        # plot_bgcolor='cadetblue',
+        font={'color': 'black'},
         )
     return fig
