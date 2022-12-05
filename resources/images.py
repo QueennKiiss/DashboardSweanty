f627616fd290189ed74943a27bfd17d7f4b4ea24
mmaestro <mmaestro@circontrol.com>
2022-12-05 12:16:37 +0100

    Refactor: rename from icon to logo

--- resources/images.py
+++ resources/images.py
@@ -4,5 +4,5 @@ IMAGES_FOLDER = "img"
 
 # Images and Icons
 USER_PROFILE_IMG = f"{IMAGES_FOLDER}/user_profile.png"
-SWEANTY_ICON = f"{IMAGES_FOLDER}/logo_sweanty_1-184x81.png"
-BEWOLFISH_ICON = f"{IMAGES_FOLDER}/bewolfish_logo_color.png"
+SWEANTY_LOGO = f"{IMAGES_FOLDER}/logo_sweanty_1-184x81.png"
+BEWOLFISH_LOGO = f"{IMAGES_FOLDER}/bewolfish_logo_color.png"
