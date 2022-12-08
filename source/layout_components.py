67ac15e4cb350506403d729412e4c1f3668b188f
mmaestro <mmaestro@circontrol.com>
2022-12-08 21:55:40 +0100

    Feat: Implement new tab content

--- source/layout_components.py
+++ source/layout_components.py
@@ -7,10 +7,32 @@ from resources import images
 import dash_bootstrap_components as dbc
 
 
-def tab1_content(app: Dash) -> html.Div:
-    user_profile_img = app.get_asset_url("img/user_profile.png")
+def create_new_session(app: Dash) -> html.Div:
     return html.Div(
-        className='tab1_content',
+        className='create_new_session',
+        children=[
+            html.Div(
+                className='form_fields_container',
+                children=[
+                    html.P(className='form_title', children=["Session name:"]),
+                    dcc.Input(
+                        className='form_field',
+                        id="session_name_input",
+                        children=["Session name"],
+                        placeholder="Enter the session name"),
+                    ],
+                ),
+            html.Button(
+                children='Create New Session',
+                className='register_user_button',
+                id='new_session_button',
+                n_clicks=0,),
+            ])
+
+
+def register_new_user(app: Dash) -> html.Div:
+    return html.Div(
+        className='register_new_user',
         children=[
             html.Div(
                 className='form_fields_container',
@@ -18,8 +40,10 @@ def tab1_content(app: Dash) -> html.Div:
                     html.P(className='form_title', children=["User name:"]),
                     dcc.Input(
                         className='form_field',
-                        children=["User name"],
-                        placeholder="Enter the name"),
+                        id='user_name_field',
+                        placeholder="Enter the name",
+                        type="text"
+                        ),
                     ],
                 ),
             html.Div(
@@ -28,8 +52,9 @@ def tab1_content(app: Dash) -> html.Div:
                     html.P(className='form_title', children=["User surnames:"]),
                     dcc.Input(
                         className='form_field',
-                        children=["User surnames"],
-                        placeholder="Enter the surnames"),
+                        id='user_surname_field',
+                        placeholder="Enter the surnames",
+                         type="text"),
                         ],
                 ),
             html.Div(
@@ -38,8 +63,9 @@ def tab1_content(app: Dash) -> html.Div:
                     html.P(className='form_title', children=["User age:"]),
                     dcc.Input(
                         className='form_field',
-                        children=["User age"],
-                        placeholder="Enter the age"),
+                        id='user_age_field',
+                        placeholder="Enter the age",
+                        type="number"),
                     ]
                 ),
             html.Div(
@@ -48,21 +74,23 @@ def tab1_content(app: Dash) -> html.Div:
                     html.P(className='form_title', children=["User sport:"]),
                     dcc.Input(
                         className='form_field',
-                        children=["User sport"],
-                        placeholder="Enter the sport"),
+                        id='user_sport_field',
+                        placeholder="Enter the sport",
+                        type="text"),
                     ],
                 ),
             html.Button(
                 children='Register New User',
+                id='register_user_button',
                 className='register_user_button',
                 n_clicks=0),
             ])
 
 
-def tab2_content(app: Dash) -> html.Div:
+def select_user_info(app: Dash) -> html.Div:
     user_profile_img = app.get_asset_url(images.USER_PROFILE_IMG)
     return html.Div(
-        className='tab2_content',
+        className='select_user_info',
         children=[
             html.Img(
                 className="user_image",
@@ -128,7 +156,7 @@ def header(app: Dash) -> html.Div:
                             html.P(
                                 className='weather_value',
                                 id='current_datetime',
-                                children=f"{datetime.now().hour}:{datetime.now().minute}:{datetime.now().second}"
+                                children=f"{datetime.now().hour:02d}:{datetime.now().minute:02d}:{datetime.now().second:02d}"
                                 ),
                             ],
                         ),
@@ -258,22 +286,43 @@ def left_column(app: Dash) -> html.Div:
         className='leftcolumn',
         children=[
             dbc.Tabs(
+                id='custom-tabs-container-id',
                 className='custom-tabs-container',
+                active_tab='new_session_tab_id',
                 children=[
                     dbc.Tab(
+                        id='new_session_tab',
+                        tab_id='new_session_tab_id',
                         tab_class_name='custom-tab',
                         label_class_name='custom-title-tab',
                         active_tab_class_name='custom-tab--selected',
                         active_label_class_name='custom-title-tab--selected',
-                        children=[tab1_content(app)],
-                        label='New User'),
+                        children=[create_new_session(app)],
+                        label='Create session',
+                        disabled=False,
+                        ),
                     dbc.Tab(
+                        id='new_user_tab',
+                        tab_id='new_user_tab_id',
                         tab_class_name='custom-tab',
                         label_class_name='custom-title-tab',
                         active_tab_class_name='custom-tab--selected',
                         active_label_class_name='custom-title-tab--selected',
-                        children=[tab2_content(app)],
-                        label='User Profile'),
+                        children=[register_new_user(app)],
+                        label='New User',
+                        disabled=False,
+                        ),
+                    dbc.Tab(
+                        id='user_profile_tab',
+                        tab_id='user_profile_tab_id',
+                        tab_class_name='custom-tab',
+                        label_class_name='custom-title-tab',
+                        active_tab_class_name='custom-tab--selected',
+                        active_label_class_name='custom-title-tab--selected',
+                        children=[select_user_info(app)],
+                        label='User Profile',
+                        disabled=False,
+                        ),
                     ],
                 ),
             ],
